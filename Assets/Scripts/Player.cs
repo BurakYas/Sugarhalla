6b3a950cedcfc71ef35af8fa8dc335c6b90c6346
BurakYas <burakyasar749@gmail.com>
2025-03-19 13:22:06 +0300

    Movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,35 +1,91 @@
+using System.Collections;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float speed = 10.0f;
+    [SerializeField] private float rollSpeed = 5;
+    [SerializeField] private float jumpForce = 10;
+    private bool isMoving;
+    private bool isGrounded;
+    private Rigidbody rb;
+    private Vector3 currentDirection;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        rb = GetComponent<Rigidbody>();
+        rb.linearDamping = 1; // Add some drag to smooth out the movement
     }
 
-    // Update is called once per frame
     void Update()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
-
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-        movement = movement.normalized * speed * Time.deltaTime;
-
-        if (movement != Vector3.zero)
+        if (isGrounded)
         {
-            // Move the cube
-            transform.Translate(movement, Space.World);
+            if (Input.GetKey(KeyCode.A))
+            {
+                currentDirection = Vector3.left;
+                Assemble(Vector3.left);
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                currentDirection = Vector3.right;
+                Assemble(Vector3.right);
+            }
+            else if (Input.GetKey(KeyCode.W))
+            {
+                currentDirection = Vector3.forward;
+                Assemble(Vector3.forward);
+            }
+            else if (Input.GetKey(KeyCode.S))
+            {
+                currentDirection = Vector3.back;
+                Assemble(Vector3.back);
+            }
+        }
 
-            // Calculate the rotation axis and angle
-            Vector3 rotationAxis = Vector3.Cross(Vector3.up, movement).normalized;
-            float angle = movement.magnitude * (180 / Mathf.PI) / (transform.localScale.x / 2);
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            Jump();
+        }
+    }
 
-            // Apply the rotation
-            transform.RotateAround(transform.position - movement / 2, rotationAxis, angle);
+    private void Assemble(Vector3 dir)
+    {
+        if (isMoving) return;
+
+        var anchor = transform.position + (Vector3.down + dir) * 0.5f; // Center of the cube
+        var axis = Vector3.Cross(Vector3.up, dir); // Axis of rotation
+        StartCoroutine(Roll(anchor, axis)); 
+    }
+
+    private IEnumerator Roll(Vector3 anchor, Vector3 axis)
+    {
+        isMoving = true;
+        float rotationAngle = 0;
+        while (rotationAngle < 90)
+        {
+            float rotationStep = rollSpeed * Time.deltaTime * 90;
+            transform.RotateAround(anchor, axis, rotationStep);
+            rotationAngle += rotationStep;
+            yield return null;
+        }
+        isMoving = false;
+    }
+
+    private void Jump()
+    {
+        if (rb != null)
+        {
+            Vector3 jumpVelocity = currentDirection * rollSpeed + Vector3.up * jumpForce;
+            rb.linearVelocity = jumpVelocity;
+            isGrounded = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
         }
     }
 }
